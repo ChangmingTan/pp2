fb399f50d3dc0ecbac42c871f85ae85d40b44bc9
Changming Tan <ctan9@mail.greenriver.edu>
2020-04-17 17:22:11 -0400

    Added step 1

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/* Name: Changming Tan
+   Date: 4/17/2020
+   URL: https://ctan.greenriverdev.com/328/pair-program/pp2/index.php
+*/
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 2</title>
+</head>
+<body>
+<h1>Pair Program 2</h1>
+<?php
+echo "<h2>PHP Array Practice.</h2>"
+?>
+</body>
+</html>
