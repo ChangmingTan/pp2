176c59b718d41d33c540db9959865faaece38030
Changming Tan <ctan9@mail.greenriver.edu>
2020-04-19 00:43:23 -0400

    Added step 5

--- functions.php
+++ functions.php
@@ -7,7 +7,6 @@ function printArr($numbers)
     }
 }
 
-//Print the array
 printArr($numbers);
 
 echo "<h2>Step 4:</h2>";
@@ -32,5 +31,22 @@ function largest($numbers)
     return $max;
 }
 
-echo "<br>The largest number in the array is " . largest($numbers);
+echo "<br>The largest number in the array is " . largest($numbers) . "<br>";
+
+echo "<h2>Step 5:</h2>";
+echo "<b>In your functions file, define a function called removeDups() that takes an array as a
+parameter and returns an array with duplicates removed. So, given the \$numbers array,
+removeDups() would return [7, 9, 8, 6]. </b><br>";
+
+function removeDups($numbers)
+{
+    //The array_unique() is used to remove duplicate values from an array
+    $result = array_unique($numbers);
+    foreach ($result as $item) {
+        echo "<br>$item";
+    }
+}
+
+removeDups($numbers);
+
 ?>
